fbf7ae631a8c77fef9f0a4284e40f4c8945d4c85
Ali Mohseni <mohseni676@yahoo.com>
2020-10-14 16:18:12 +0330

    add payment options

--- ussd/settings.py
+++ ussd/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'ussd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ussd',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
